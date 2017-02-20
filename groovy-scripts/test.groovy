f02c691ca4a536306c02890a04c82d9329a82416
Tyrelle Chetty <tyrelle.chetty@standardbank.co.za>
2017-02-20 20:53:46 +0200

    edited script to get output

--- groovy-scripts/test.groovy
+++ groovy-scripts/test.groovy
@@ -21,7 +21,7 @@ def test_out = [
   pathToYMLFile : "http://stash.standardbank.co.za:7990/projects/CBDEV/repos/feature-team-config/raw/sanity-config.yml",
   featureTeam : "SANITY",
   username : "AO_USR",
-  password : "Supp0rt!"
+  password : "Supp0rt!",
   status : "failed"
 ]
 
